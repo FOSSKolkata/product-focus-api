3ee71903727380e15cdc5136a845d7efcb88767c
tapas-banerjee <74599086+tapas-banerjee@users.noreply.github.com>
2021-04-06 19:50:20 +0530

    IQuery and ICommand interface added

---
+++ Common/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+
+namespace Common
+{
+    public interface ICommand
+    {
+    }
+
+    public interface ICommandHandler<TCommand>
+        where TCommand : ICommand
+    {
+        Result Handle(TCommand command);
+    }
+}
